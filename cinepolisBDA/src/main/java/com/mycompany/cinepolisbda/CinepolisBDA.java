c9cf703d7ba1dbcc582a27ca3c43493f987fcc10
ReneEzequiel23 <150867485+ReneEzequiel23@users.noreply.github.com>
2024-09-22 05:15:27 -0700

    cinepolis

---
+++ cinepolisBDA/src/main/java/com/mycompany/cinepolisbda/CinepolisBDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cinepolisbda;
+
+/**
+ *
+ * @author filor
+ */
+public class CinepolisBDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
